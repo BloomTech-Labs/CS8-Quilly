0509bd10061d7cceb3005a4a1db5bf8f0f8fdd8b
Kenny <khpham24@gmail.com>
2018-07-18 11:24:28 -0600

    Added notes to code and finished settings page

---
+++ frontend/src/components/billing/billing.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Billing extends Component {
+  render() {
+    return 
+    <div className="App"> 
+      
+    </div>;
+  }
+}
+
+export default Billing;
