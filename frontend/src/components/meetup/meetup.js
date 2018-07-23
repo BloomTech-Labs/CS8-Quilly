1a6a6d953a910c6abda62a9c1382bb462a052a9c
kiadorii <sgchoi529@email.arizona.edu>
2018-07-23 00:03:59 -0700

    Rendering temp data and w state - meetup

--- frontend/src/components/meetup/meetup.js
+++ frontend/src/components/meetup/meetup.js
@@ -1,57 +1,116 @@
-import React, { Component } from 'react';
-import './meetup.css';
+import React, { Component } from "react";
+import "./meetup.css";
+
+let fakeServerData = {
+  meetups: [
+    {
+      date: "5/10/2018",
+      title: "Blog Post",
+      link: "www.google.com",
+      notes: "Retweeted"
+    },
+    {
+      date: "5/11/2018",
+      title: "Blof Post",
+      link: "www.goofle.com",
+      notes: "Retweefed"
+    }
+  ]
+};
 
 class Meetup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
+  constructor() {
+    super();
+
+    this.state = {
+      serverData: {},
+      dateInput: "",
+      titleInput: "",
+      linkInput: "",
+      notesInput: "",
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentWillMount() {
+    this.setState({ serverData: fakeServerData });
+  }
+
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    console.log(`This is the stateee ${this.state}`);
+    // alert("Contribution Submitted: " + this.state.value);
+    // $.post("/submitFormData", {data: this.state.dateInput...}, function(res) {
+
+    // })
     event.preventDefault();
   }
 
   render() {
+    console.log(this.state);
     return (
-      <div className="Meetup">
-        <form onSubmit={this.handleSubmit}>
+      <div className="MeetupComponents">
+        <div className="meetupss">
+          {/* Displaying over user's contributions -- will display nothing if no input given */}
+          {this.state.serverData.meetups.map(function(meetup) {
+            return (
+              <div key={meetup.title}>
+                <div className="date">{meetup.date}</div>
+                <div className="title">{meetup.title}</div>
+                <div className="notes">{meetup.notes}</div>
+              </div>
+            );
+          })}
+        </div>
+
+        {/* Form Component */}
+        <div className="MeetupForm">
+          <form onSubmit={this.handleSubmit}>
             <input
-              className="Date"
+              className="formDate"
+              required="true"
               type="date"
-              value={this.state.value}
+              name="dateInput"
+              value={this.state.dateInput}
               onChange={this.handleChange}
             />
             <input
-              className="Activity"
+              className="formActivity"
               type="text"
-              placeholder="Activity"
-              value={this.state.value}
+              placeholder="Meetup"
+              required="true"
+              name="titleInput"
+              value={this.state.titleInput}
               onChange={this.handleChange}
             />
             <input
-              className="Link"
+              className="formLink"
               type="text"
               placeholder="Link"
-              value={this.state.value}
+              required="true"
+              name="linkInput"
+              value={this.state.linkInput}
               onChange={this.handleChange}
             />
             <input
               className="Notes"
               type="text"
               placeholder="Notes"
-              value={this.state.value}
+              name="notesInput"
+              value={this.state.notesInput}
               onChange={this.handleChange}
             />
-          <input type="submit" value="Submit" />
-        </form>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
