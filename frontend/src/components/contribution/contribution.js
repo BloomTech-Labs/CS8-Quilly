fab047fde47b0c5348a699975f5975ecd47ba04a
kiadorii <sgchoi529@email.arizona.edu>
2018-07-23 00:00:16 -0700

    Contribution state managed w/ temp data

--- frontend/src/components/contribution/contribution.js
+++ frontend/src/components/contribution/contribution.js
@@ -69,12 +69,18 @@ export default Contribution;
 
 let fakeServerData = {
   contributions: [
-    ({
+    {
       date: "5/10/2018",
       title: "Blog Post",
       link: "www.google.com",
       notes: "Retweeted"
-    })
+    },
+    {
+      date: "5/11/2018",
+      title: "Blof Post",
+      link: "www.goofle.com",
+      notes: "Retweefed"
+    }
   ]
 };
 
@@ -83,12 +89,35 @@ class Contribution extends Component {
     super();
 
     this.state = {
-      serverData: {}
+      serverData: {},
+      dateInput: "",
+      titleInput: "",
+      linkInput: "",
+      notesInput: "",
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount() {
     this.setState({ serverData: fakeServerData });
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(`This is the stateee ${this.state}`);
+    // alert("Contribution Submitted: " + this.state.value);
+    // $.post("/submitFormData", {data: this.state.dateInput...}, function(res) {
+
+    // })
+    event.preventDefault();
   }
 
   render() {
@@ -96,17 +125,58 @@ class Contribution extends Component {
     return (
       <div className="ContributionComponents">
         <div className="contributions">
-        { this.state.serverData.contributions.map(function(contribution) {
-         return( 
-         <div key={contribution.title}>
-            <div className="date">{contribution.date}</div>
-            <div className="title">{contribution.title}</div>
-            <div className="notes">{contribution.notes}</div>
-        </div>)
-        })}
+          {/* Displaying over user's contributions -- will display nothing if no input given */}
+          {this.state.serverData.contributions.map(function(contribution) {
+            return (
+              <div key={contribution.title}>
+                <div className="date">{contribution.date}</div>
+                <div className="title">{contribution.title}</div>
+                <div className="notes">{contribution.notes}</div>
+              </div>
+            );
+          })}
         </div>
 
-        <div className="ContributionForm" />
+        {/* Form Component */}
+        <div className="ContributionForm">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              className="formDate"
+              required="true"
+              type="date"
+              name="dateInput"
+              value={this.state.dateInput}
+              onChange={this.handleChange}
+            />
+            <input
+              className="formActivity"
+              type="text"
+              placeholder="Contribution"
+              required="true"
+              name="titleInput"
+              value={this.state.titleInput}
+              onChange={this.handleChange}
+            />
+            <input
+              className="formLink"
+              type="text"
+              placeholder="Link"
+              required="true"
+              name="linkInput"
+              value={this.state.linkInput}
+              onChange={this.handleChange}
+            />
+            <input
+              className="Notes"
+              type="text"
+              placeholder="Notes"
+              name="notesInput"
+              value={this.state.notesInput}
+              onChange={this.handleChange}
+            />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
