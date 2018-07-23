187fad7c5a1eb8b5245e13035c799e080749fa19
kiadorii <sgchoi529@email.arizona.edu>
2018-07-22 21:47:59 -0700

    Inital display of temp data - contribution.js

--- frontend/src/components/contribution/contribution.js
+++ frontend/src/components/contribution/contribution.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import './contribution.css';
 
+let fakeServerData = {
+  contribution: { 
+    date: 'May 1, 1000'
+  },
+};
+
 class Contribution extends Component {
   constructor(props) {
     super(props);
@@ -15,24 +21,28 @@ class Contribution extends Component {
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('Contribution Submitted: ' + this.state.value);
     event.preventDefault();
   }
 
   render() {
     return (
       <div className="Contribution">
+      <div>{fakeServerData.contribution.date}</div>
+      <div className="ContributionForm">
         <form onSubmit={this.handleSubmit}>
             <input
               className="Date"
               type="date"
               value={this.state.value}
+              required="true"
               onChange={this.handleChange}
-            />
+              />
             <input
               className="Activity"
               type="text"
               placeholder="Contribution"
+              required="true"
               value={this.state.value}
               onChange={this.handleChange}
             />
@@ -40,19 +50,22 @@ class Contribution extends Component {
               className="Link"
               type="text"
               placeholder="Link"
+              required="true"
               value={this.state.value}
               onChange={this.handleChange}
-            />
+              />
             <input
               className="Notes"
               type="text"
               placeholder="Notes"
+              required="true"
               value={this.state.value}
               onChange={this.handleChange}
-            />
+              />
           <input type="submit" value="Submit" />
         </form>
       </div>
+    </div>
     );
   }
 }
