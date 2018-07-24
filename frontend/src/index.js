bd8a7e626259d8af1079c6f9f550ddfaf1577935
Robert Rak <robertrak@hotmail.com>
2018-07-24 18:44:26 -0400

    added initial stripe front-end

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,7 @@ import Meetupspage from './pages/meetups';
 import Contributionspage from './pages/contributions';
 import Billingpage from './pages/billing';
 import Settingspage from './pages/settings';
+import { StripeProvider } from 'react-stripe-elements';
 
 ReactDOM.render(
   <Router>
@@ -16,7 +17,9 @@ ReactDOM.render(
       <Route path="/jobs" component={Joblistpage} />
       <Route path="/meetups" component={Meetupspage} />
       <Route path="/contributions" component={Contributionspage} />
+      <StripeProvider apiKey="pk_test_K1tJV1QhjRPnqQFwDFxe6vZd">
       <Route path="/billing" component={Billingpage} />
+      </StripeProvider>
       <Route path="/settings" component={Settingspage} />
     </div>
   </Router>,
