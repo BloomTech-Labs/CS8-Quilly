448d78769baf6b2a9002c5ddefdf35f02452d953
joshuacoyne <josh.j.coyne@gmail.com>
2018-07-25 17:16:03 -0400

    updated login and account signup modals

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,8 +3,11 @@ module.exports = {
   db: process.env.DBURL || 'mongodb://localhost:27017/quilly',
   sessionSecret: process.env.SESSION_SECRET || 'change this',
   corsOptions: {
-    origin: process.env.CLIENTURL || "*",
-    optionsSuccessStatus: 200,
+    // origin: process.env.CLIENTURL || "http://localhost:3000",
+    // optionsSuccessStatus: 200,
+    origin:true,
+    methods:['GET','POST','PUT','DELETE'],
+    credentials: true
   },  
   stripe: {
     secret_key: process.env.STRIPE_SECRET_KEY || 'sk_test_QixOiUfMKS32WljW9ThkIi1e',
