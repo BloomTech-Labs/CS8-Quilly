4416a36f2b73846056941d7e4e641dbfb6d8485b
kiadorii <sgchoi529@email.arizona.edu>
2018-08-06 14:10:09 -0700

    Reverting to previous config

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,7 +2,6 @@ module.exports = {
   port: 3000,
   serverUrl: process.env.NODE_ENV === "production" ? "https://mighty-hollows-20066.herokuapp.com" : "http://localhost:5000",
   stripe: {
-    // secretKey: process.env.STRIPE_KEY || 'sk_test_QixOiUfMKS32WljW9ThkIi1e',
-    publicKey: process.env.NODE_ENV === "production" ? "" : "pk_test_K1tJV1QhjRPnqQFwDFxe6vZd",
+    publicKey: process.env.NODE_ENV === "production" ? "pk_live_rkM9rtRZKBLaFRUTHUX3W71X" : "pk_test_K1tJV1QhjRPnqQFwDFxe6vZd",
   },
 };
