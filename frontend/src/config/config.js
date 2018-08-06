ecbb9c79b7abf418757cf5ea2476b5d9ea4cdde5
kiadorii <sgchoi529@email.arizona.edu>
2018-08-06 11:25:11 -0700

    Trying to get netlify to deploy properly

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,7 +2,16 @@ module.exports = {
   port: 3000,
   serverUrl: process.env.NODE_ENV === "production" ? "https://mighty-hollows-20066.herokuapp.com" : "http://localhost:5000",
   stripe: {
-    apiKey: process.env.STRIPE_KEY || 'sk_test_QixOiUfMKS32WljW9ThkIi1e', // Secret Key
-    publicKey: process.env.NODE_ENV === "production" ? "" : "pk_test_K1tJV1QhjRPnqQFwDFxe6vZd",
-  },
+    apiKey: process.env.STRIPE_KEY || 'sk_test_QixOiUfMKS32WljW9ThkIi1e',
+    stripePubKey:
+      process.env.STRIPE_PUB_KEY  || 'pk_test_K1tJV1QhjRPnqQFwDFxe6vZd',
+    defaultPlan: 'default',
+    plans: ['default'],
+    planData: {
+      default: {
+        name: 'Default',
+        price: 4.99
+      }
+    }
+  }
 };
