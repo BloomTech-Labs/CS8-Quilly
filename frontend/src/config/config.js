e5d5fa2b276439a58bea8dfacfe3e9d4847353a4
kiadorii <sgchoi529@email.arizona.edu>
2018-08-06 11:40:27 -0700

    config.js in frontend

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -3,6 +3,6 @@ module.exports = {
   serverUrl: process.env.NODE_ENV === "production" ? "https://mighty-hollows-20066.herokuapp.com" : "http://localhost:5000",
   stripe: {
     apiKey: process.env.STRIPE_KEY || 'sk_test_QixOiUfMKS32WljW9ThkIi1e', // Secret Key
-    stripePubKey: process.env.NODE_ENV === "production" ? "" : "pk_test_K1tJV1QhjRPnqQFwDFxe6vZd",
+    publicKey: process.env.NODE_ENV === "production" ? "" : "pk_test_K1tJV1QhjRPnqQFwDFxe6vZd",
   },
 };
