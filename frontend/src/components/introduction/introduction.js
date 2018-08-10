45c3f25e7b8eca19bd9d9ecf7d65b4349b964b7d
kiadorii <sgchoi529@email.arizona.edu>
2018-08-10 11:29:12 -0700

    Introduction Image - Initial Commit

--- frontend/src/components/introduction/introduction.js
+++ frontend/src/components/introduction/introduction.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './introduction.css';
+
 import imgKia from '../../img/Self-Kia.jpg';
+import imgKenny from '../../img/Self-Kenny.jpg';
+import imgJosh from '../../img/Self-Josh.jpg';
+import imgRobert from '../../img/Self-Robert.jpg';
 
 class Introductions extends Component {
     render() {
         return (
             <div>
                 <div class="groupMembers">
-                    <imgKia />
+                    <img src={imgKia} />
+                    <img src={imgKenny} />
+                    <img src={imgJosh} />
+                    <img src={imgRobert} />
                 </div>
             </div>
         );
